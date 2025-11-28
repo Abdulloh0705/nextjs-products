7c33c1b0a50853e345a116bd5226eb9b905cae40
Abdulloh <abdullohxojiakbarov674@gmail.com>
2025-11-28 15:19:45 +0500

    Add initial migration for production DB1

---
+++ prisma/migrations/20251128101836_init/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "brand" TEXT NOT NULL DEFAULT 'Unknown',
+    "description" TEXT NOT NULL DEFAULT '',
+    "rating" DOUBLE PRECISION NOT NULL DEFAULT 4.5,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
